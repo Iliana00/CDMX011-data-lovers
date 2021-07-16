fd4aa681db47d474f02f8fc2291fff979be85825
Iliana00 <carrasquedo.279@gmail.com>
2021-07-16 18:33:28 -0500

    Se creó la primera función, ya devueleve en consola los títulos y posters

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+/*import studioGhibli from './data.js';
+// import data from './data.js';
+import data from './data/ghibli/ghibli.js';
+// import data from './data/ghibli/ghibli.js';
+var films = studioGhibli.films;
+var posters = studioGhibli.posters;
+const container = document.getElementById('container');
+    for(let i = 0; i < films.length; i++){
+        console.log("dbckasdb")
+        const myDiv = document.createElement('div');
+        myDiv.textContent = films[i];
+        document.body.appendChild(myDiv);
+       myDiv.textContent =
+       
+    }
+window.addEventListener("load", prueba()) 
 
-console.log(example, data);
+console.log(typeof(films()), posters());*/
+
+import studioGhibli from './data.js';
+// import data from './data.js';
+import data from './data/ghibli/ghibli.js';
+// import data from './data/ghibli/ghibli.js';
+let films = studioGhibli.films;
+let posters = studioGhibli.posters;
+const container = document.getElementById('container');
+    //console.log(Object.keys[films]);
+    console.log(films)
+
+//console.log(typeof(films()), posters());
+
