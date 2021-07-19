35749390dd2da0ea3cb3a05f2966d66b1b4a98ea
Iliana00 <carrasquedo.279@gmail.com>
2021-07-19 15:01:25 -0500

    Cambios hechos en la mañana del 19 de julio

--- src/main.js
+++ src/main.js
@@ -4,16 +4,20 @@ let films = studioGhibli.films();
 let posters = studioGhibli.posters();
 let container = document.getElementById('container');
 
+
 for (let i = 0; i < films.length; i++) {
     const myDiv = document.createElement('div');
-    myDiv.textContent = films[i];
+    const paragraph = document.createElement('p');
+    myDiv.appendChild(paragraph)
+    paragraph.textContent = films[i];
     container.appendChild(myDiv).className = "item";    
 }
-
+const myDivs = document.getElementsByClassName("item");
 for (let i = 0; i < posters.length; i++) {
-    const myImg = document.createElement('img');
-    myImg.innerHTML = posters[i];
-    container.appendChild(myImg).className = "img";
-
+    const img = document.createElement("img");    
+    let attr = document.createAttribute("src");
+    attr.value = posters[i];
+    img.setAttributeNode(attr);
+    myDivs[i].appendChild(img).className = "img";
 }
 
