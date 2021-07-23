e1b8a1d83ff5a22b2f408ff9b406b1d320492e45
Iliana00 <carrasquedo.279@gmail.com>
2021-07-23 10:32:10 -0500

    Cambios 23 de julio

--- src/data.js
+++ src/data.js
@@ -6,14 +6,15 @@ export function home() {
     return {
       title: movie.title,
       poster: movie.poster,
-      release_date : movie.release_date,
-      rt_score : movie.rt_score      
+      release_date: movie.release_date,
+      rt_score: movie.rt_score
     }
   })
 
   return array;
 }
 
+<<<<<<< HEAD
 
 export function order(unordered, ascOrDesc) {  
   console.log(array)  
@@ -29,42 +30,79 @@ export function order(unordered, ascOrDesc) {
   if (ascOrDesc == "asc") {
     return unordered.sort(function (a, b) {
       return a - b;
+=======
+export function order(unordered, ascOrDesc) {
+  let answer = "No ha hecho nada";
+  if (unordered == "score" && ascOrDesc == "asc") {
+    unordered = array;
+    console.log(unordered);
+    answer = array.sort(function (a, b) {
+      return a.rt_score - b.rt_score;
+>>>>>>> bed79318e566436b81f530a4c23684f321f24e97
     });
-  } else if (ascOrDesc == "desc") {
-    return unordered.sort(function (a, b) {
-      return b - a;
+  } else if (unordered == "score" && ascOrDesc == "desc") {
+    unordered = array;
+    console.log(unordered);
+    answer = array.sort(function (a, b) {
+      return b.rt_score - a.rt_score;
     });
+  } else if (unordered == "year" && ascOrDesc == "asc") {
+    unordered = array;
+    console.log(unordered);
+    answer = array.sort(function (a, b) {
+      return a.release_date - b.release_date;
+    });
+  } else if (unordered == "year" && ascOrDesc == "desc") {
+    unordered = array;
+    console.log(unordered);
+    answer = array.sort(function (a, b) {
+      return b.release_date - a.release_date;
+    });
+  } else {
+    answer = sortTitle(unordered, ascOrDesc);
+    console.log(answer);
   }
+  return answer;
 }
 
 function sortTitle(unordered, ascOrDesc) {
+  let answer = "";
+  unordered = array;
+  console.log(unordered);
+  if (ascOrDesc == "asc") {
+    answer = array.sort();
+  } else {
+    answer = array.sort().reverse();
+  }
+  return answer
+}
+
+
+// ------------------------------------------------
+
+/*if (unordered == "score") {
+  unordered = data.films.map(x => x.rt_score);
+} else if (unordered == "year") {
+  unordered = data.films.map(x => x.release_date);
+} else {
+  unordered = data.films.map(x => x.title);
+  return sortTitle(unordered, ascOrDesc);
+}
+if (ascOrDesc == "asc") {
+  return unordered.sort(function (a, b) {
+    return a - b;
+  } else if (ascOrDesc == "desc") {
+    return unordered.sort(function (a, b) {
+      return b - a;
+    })
+  })
+}*/
+
+function sortTitle(unordered, ascOrDesc) {
+  unordered = data.films.map(x => x.title);
   if (ascOrDesc == "asc") {
     return unordered.sort();
   } else {
     return unordered.sort().reverse();
   }
 }
-//console.log("ascendingOrder")
-
-/*
-export function score(rtScore) {
-  //const rtScore = home().rt_score;
-  return rtScore.sort(function(a, b) {
-    //console.log(rt_score)
-    return a - b;
-  });
-  
-}
-*/
-
-/*function ascendingOrder(arr) {
-  return arr.sort(function(a, b) {
-    return a - b;
-  });
-}
-ascendingOrder([1, 5, 2, 3, 4]);*/
-
-
-/* export const azfilter{
-   const array = data
- }*/
--- src/main.js
+++ src/main.js
@@ -1,48 +1,51 @@
-import { home } from './data.js';
+import {
+  home
+} from './data.js';
 //import { score } from './data.js';
-import { order } from './data.js';
+import {
+  order
+} from './data.js';
 
 
 document.getElementById("firstFilter")
-    .addEventListener("change", function prueba (e) {
-        var answer = "";
-        let ascOrDesc = "";
-        let unordered = "";
-        switch(e.target.value) {            
-            case "mejor-val":   
-            ascOrDesc = "asc";
-            unordered = "score";
-            answer = order(unordered, ascOrDesc);
-            break;
-            case "menor-val":
-            ascOrDesc = "desc";
-            unordered = "score";
-            answer = order(unordered, ascOrDesc);
-            break;
-            case "reciente":  
-            ascOrDesc = "desc";
-            unordered = "year";
-            answer = order(unordered, ascOrDesc);
-            break;
-            case "antiguo":  
-            ascOrDesc = "asc";
-            unordered = "year";
-            answer = order(unordered, ascOrDesc);
-            break;
-            case "a-z":  
-            ascOrDesc = "asc";
-            unordered = "title";
-            answer = order(unordered, ascOrDesc);
-            break;
-            case "z-a":  
-            ascOrDesc = "desc";
-            unordered = "title";
-            answer = order(unordered, ascOrDesc);
-            break;
-        }
-        console.log(answer);
-        return answer;        
-})
+  .addEventListener("change", function prueba(e) {
+    var answer = "";
+    let ascOrDesc = "";
+    let unordered = "";
+    switch (e.target.value) {
+      case "mejor-val":
+        ascOrDesc = "asc";
+        unordered = "score";
+        answer = order(unordered, ascOrDesc);
+        break;
+      case "menor-val":
+        ascOrDesc = "desc";
+        unordered = "score";
+        answer = order(unordered, ascOrDesc);
+        break;
+      case "reciente":
+        ascOrDesc = "desc";
+        unordered = "year";
+        answer = order(unordered, ascOrDesc);
+        break;
+      case "antiguo":
+        ascOrDesc = "asc";
+        unordered = "year";
+        answer = order(unordered, ascOrDesc);
+        break;
+      case "a-z":
+        ascOrDesc = "asc";
+        unordered = "title";
+        answer = order(unordered, ascOrDesc);
+        break;
+      case "z-a":
+        ascOrDesc = "desc";
+        unordered = "title";
+        answer = order(unordered, ascOrDesc);
+        break;
+    }
+    return answer;
+  })
 
 
 let myDiv = null;
@@ -50,6 +53,7 @@ let title = null;
 let score = null;
 let date = null;
 for (let i = 0; i < home().length; i++) {
+<<<<<<< HEAD
     myDiv = document.createElement('div');
     document.getElementById('container')
         .appendChild(myDiv).className = "item";
@@ -63,17 +67,31 @@ for (let i = 0; i < home().length; i++) {
     myDiv.appendChild(score).className = "score";
     score.textContent = "Rotten tomatoes score: " + home()[i].rt_score;
     //console.log(myDiv);
+=======
+  myDiv = document.createElement('div');
+  document.getElementById('container')
+    .appendChild(myDiv).className = "item";
+  title = document.createElement('p');
+  myDiv.appendChild(title).className = "title";
+  title.textContent = home()[i].title;
+  date = document.createElement('p');
+  myDiv.appendChild(date).className = "date";
+  date.textContent = home()[i].release_date;
+  score = document.createElement('p');
+  myDiv.appendChild(score).className = "score";
+  score.textContent = "Rotten tomatoes score: " + home()[i].rt_score;
+>>>>>>> bed79318e566436b81f530a4c23684f321f24e97
 }
 
 let img = null;
 let attr = null;
 for (let i = 0; i < home().length; i++) {
-    img = document.createElement("img");    
-    attr = document.createAttribute("src");
-    attr.value = home()[i].poster;
-    img.setAttributeNode(attr);
-    document.getElementsByClassName("item")[i]
-        .appendChild(img).className = "img";
+  img = document.createElement("img");
+  attr = document.createAttribute("src");
+  attr.value = home()[i].poster;
+  img.setAttributeNode(attr);
+  document.getElementsByClassName("item")[i]
+    .appendChild(img).className = "img";
 }
 
 //console.log(ascendingOrder(score))
@@ -82,8 +100,13 @@ for (let i = 0; i < home().length; i++) {
     console.log(home()[i].title);
 }*/
 
+<<<<<<< HEAD
 //console.log(home());
  //console.log(home());
 //console.log(director());
 
+=======
+>>>>>>> bed79318e566436b81f530a4c23684f321f24e97
 
+//console.log(home());
+//console.log(director());
